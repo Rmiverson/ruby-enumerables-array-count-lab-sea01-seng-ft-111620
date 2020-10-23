92331e7d4d6048eaa825432d698a4a6cf13540a0
Riley Iverson <rmiverson99@gmail.com>
2020-10-23 22:23:00 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,9 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  ans = 0
+  array.count do |String|
+    ans += 1
+  end  
 end
 
 def count_empty_strings(array)
